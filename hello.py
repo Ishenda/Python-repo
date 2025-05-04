259050bc8521c7e453ea8e897cecb92042374739
ishenda <ruttavalensitheonest@gmail.com>
2025-05-04 15:02:48 +0300

    Added hello.py

---
+++ hello.py
@@ -0,0 +1,8 @@
+import time
+print ("Helow world, Python is one of the best scripting language")
+
+myList = ["so let us","learn python", "to be come ","good hackers","Bye!!"]
+
+for i in myList:
+    print (i,end="\n")
+    time.sleep(3)
