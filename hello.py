b2ed7bdfc953516ff0a5a312477ebb4aa15a947c
ishenda <ruttavalensitheonest@gmail.com>
2025-05-04 15:17:35 +0300

    Added a print message

--- hello.py
+++ hello.py
@@ -6,3 +6,5 @@ myList = ["so let us","learn python", "to be come ","good hackers","Bye!!"]
 for i in myList:
     print (i,end="\n")
     time.sleep(3)
+
+print ("\nLet us Enjoy scripting programming")
